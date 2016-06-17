36eb36d96ca955cb31074cb1c1973fad2bd14d1b
Miguh Ruiz <miguhruiz@gmail.com>
2016-06-17 23:09:28 +0200

    [Added] Add email route and its funtionality

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import mc from 'mailchimp-api/mailchimp'
+import config from './config'
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.static('./public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.set('views', './views')
 app.set('view engine', 'pug')
 
+const mail = new mc.Mailchimp(config.mailchimp.api_key)
+
 app.get("/", (req, res) => {
   res.render('index')
+})
+
+app.post("/new-email", (req, res) => {
+  mail.lists.subscribe({id: config.mailchimp.listid, email: {email: req.body.email}}, function (data) {
+    console.log(data)
+  }), function (error) { if(error != []){ console.log(error) } }
 })
 
 app.listen(port, () => {
